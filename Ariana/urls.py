53cfde878a23032c87a3a4f01679c8682ddf73c7
jibinmathew <jibinmathew.69@gmail.com>
2018-12-04 13:01:36 +0530

    Fix chat app url

--- Ariana/urls.py
+++ Ariana/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('questionaire.urls')),
-    path('chat',include('chat.urls')),
+    path('chat/',include('chat.urls')),
     path('admin/', admin.site.urls),
 ]
