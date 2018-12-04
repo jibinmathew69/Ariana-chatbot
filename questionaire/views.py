3c2307ad99ba4fca949f51220d354135ac341bb2
jibinmathew <jibinmathew.69@gmail.com>
2018-12-04 08:24:58 +0530

    Add views - questinaire app

--- questionaire/views.py
+++ questionaire/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .serializers import QuestionaireSerializer,QuestionsSerializer,ResponsesSerializer
+from .models import Questionaire,Questions,Responses
+from rest_framework import viewsets
 
-# Create your views here.
+class QuestionaireView(viewsets.ModelViewSet):
+    queryset = Questionaire.objects.all()
+    serializer_class = QuestionaireSerializer
+
+
+class QuestionsView(viewsets.ModelViewSet):
+    queryset = Questions.objects.all()
+    serializer_class = QuestionaireSerializer
+
+
+class ResponsesView(viewsets.ModelViewSet):
+    queryset = Responses.objects.all()
+    serializer_class = QuestionaireSerializer
+
+
+
