d1093f7eab366c956ff4d0aa6d8a423388365b02
jibinmathew <jibinmathew.69@gmail.com>
2018-12-04 12:51:43 +0530

    Add chat app url in project url

--- Ariana/urls.py
+++ Ariana/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('questionaire.urls')),
+    path('chat/',include('chat.urls')),
     path('admin/', admin.site.urls),
 ]
