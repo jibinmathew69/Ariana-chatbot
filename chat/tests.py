3932ca89268929a850b01796869ff89ac9787086
jibinmathew <jibinmathew.69@gmail.com>
2018-12-04 17:18:31 +0530

    Test cases chatbot

--- chat/tests.py
+++ chat/tests.py
@@ -87,3 +87,134 @@ class ChatTreeViewTest(TestCase):
 
         self.assertEqual(len(response.json()),count)
 
+
+
+
+class ChatbotTest(TestCase):
+
+    def setUp(self):
+        file_path = "chat/test_data/demo.json"
+        file_obj = open(file_path)
+        file_obj.seek(0)
+        url = reverse('chattree')
+
+        data = {
+            'file': file_obj,
+            'name': 'Food choices'
+        }
+
+        self.client.post(url, data)
+
+
+    def test_query_completion(self):
+        queries = [
+            "",
+            "Yes",
+            "Pizza",
+            "Yes",
+            ""
+        ]
+
+        url = reverse('chatbot')
+
+        for query in queries:
+            response = self.client.post(url, {
+                'message': query,
+                'questionaire' : 1
+            })
+
+
+        self.assertEqual(response.json(), "Restarting Conversation: Are you hungry?->Yes->Pizza->Yes")
+
+
+    def test_query_completion2(self):
+        queries = [
+            "",
+            "Yes",
+            "Hamburger",
+            ""
+        ]
+
+        url = reverse('chatbot')
+
+        for query in queries:
+            response = self.client.post(url, {
+                'message': query,
+                'questionaire': 1
+            })
+
+
+        self.assertEqual(response.json(), "Restarting Conversation: Are you hungry?->Yes->Hamburger")
+
+
+    def test_query_completion3(self):
+        queries = [
+            "",
+            "no",
+            ""
+        ]
+
+        url = reverse('chatbot')
+
+        for query in queries:
+            response = self.client.post(url, {
+                'message': query,
+                'questionaire': 1
+            })
+
+
+        self.assertEqual(response.json(), "Restarting Conversation: Are you hungry?->no")
+
+
+    def test_query_invalid_option(self):
+        queries = [
+            "",
+            "Yes",
+            "piza"
+        ]
+
+        url = reverse('chatbot')
+
+        for query in queries:
+            response = self.client.post(url, {
+                'message': query,
+                'questionaire': 1
+            })
+
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_query_invalid_questionaire(self):
+        queries = [
+            "",
+        ]
+
+        url = reverse('chatbot')
+
+        for query in queries:
+            response = self.client.post(url, {
+                'message': query,
+                'questionaire': 2
+            })
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_query_message_missing(self):
+
+
+        url = reverse('chatbot')
+
+
+        response = self.client.post(url, {
+                'questionaire': 1
+            })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+        response = self.client.post(url, {
+            'questionaire': 1
+        })
+
+        self.assertEqual(response.json(), "Invalid input")
