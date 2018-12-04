20f6a583b0d5d0325fa9dcfb959190c0c6cf004e
jibinmathew <jibinmathew.69@gmail.com>
2018-12-04 08:01:55 +0530

    Add Questionaire model

--- questionaire/models.py
+++ questionaire/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Questionaire(models.Model):
+    name = models.CharField(max_length=200,unique=True)
+
+    def __str__(self):
+        return self.name
+
+
