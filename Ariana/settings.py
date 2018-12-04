c397bf72991edd5aee4b5c945b75291099c20837
jibinmathew <jibinmathew.69@gmail.com>
2018-12-04 18:22:19 +0530

    Update setting to production mode, and allowed host

--- Ariana/settings.py
+++ Ariana/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_jq%2g0jhwad(+om8lgl2wojl_qr4^3qcpsxd@mw0^_n0$buxr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
