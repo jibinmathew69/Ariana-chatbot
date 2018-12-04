6dc7107f3012d03ba1989b38b1467d1ee24f7c19
jibinmathew <jibinmathew.69@gmail.com>
2018-12-04 08:33:27 +0530

    Add urls - questionaire app

---
+++ questionaire/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path,include
+from .views import QuestionaireView,QuestionsView,ResponsesView
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+
+router.register('questionaire',QuestionaireView)
+router.register('questions',QuestionsView)
+router.register('responses',ResponsesView)
+
+
+urlpatterns = [
+    path('',include(router))
+]
