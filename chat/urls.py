fd7eb33a9a35df802781114b5dd232cc74f86cf1
jibinmathew <jibinmathew.69@gmail.com>
2018-12-04 12:56:58 +0530

    Fix chat app urls

--- chat/urls.py
+++ chat/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register('questionaire',ChatView)
+router.register('chat',ChatView)
 
 urlpatterns = [
     path('',include(router.urls))
