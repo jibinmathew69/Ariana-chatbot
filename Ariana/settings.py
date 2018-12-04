d46d4dd29f308cdf0e2ec0fbf573d45a84076dab
jibinmathew <jibinmathew.69@gmail.com>
2018-12-04 12:50:38 +0530

    Fix missing comma installed apps

--- Ariana/settings.py
+++ Ariana/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'questionaire.apps.QuestionaireConfig'
+    'questionaire.apps.QuestionaireConfig',
     'chat.apps.ChatConfig'
 ]
 
