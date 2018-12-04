eb8a012d73475d7bb7006b235d2d511c90ba075a
jibinmathew <jibinmathew.69@gmail.com>
2018-12-04 08:14:36 +0530

    Add Chat model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from questionaire.models import Questionaire
-# Create your models here.
+
+
+class Chat(models.Model):
+    questionaire = models.ForeignKey(Questionaire, on_delete=models.CASCADE)
+    status = models.PositiveIntegerField(default=1,null=True)
+    log = models.TextField(default='')
+
+    def __str__(self):
+        return self.log
+
